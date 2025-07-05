45cb79b2e616bf8c31a9ae243791ec059bed3966
Islam Yamor <islamyamorz@gmail.com>
2025-07-05 14:37:56 +0300

    changed the path for the typings into the dist folder

--- @types/getGlobals.d.ts
+++
@@ -1,5 +0,0 @@
-export default function getGlobals(url: string): {
-    __filename: string;
-    __dirname: string;
-};
-//# sourceMappingURL=getGlobals.d.ts.map
--- @types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as getGlobals } from './getGlobals.js';
-//# sourceMappingURL=index.d.ts.map
